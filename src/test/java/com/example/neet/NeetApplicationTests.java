75e420060d0b5c7ea67b264281148873957fba43
gayathrirajaguru <gayuguru111@gmail.com>
2026-04-29 09:34:23 +0530

    fix dockerfile location

--- src/test/java/com/example/neet/NeetApplicationTests.java
+++ src/test/java/com/example/neet/NeetApplicationTests.java
@@ -2,6 +2,9 @@ package com.example.neet;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+
 
 @SpringBootTest
 class NeetApplicationTests {
